3c8b7c35eae4ce670fddc9c883ddb6270c982f67
Maksim Ryzhikov <rv.maksim@gmail.com>
2020-09-18 12:33:33 +0300

    chore: remove unused css rules from final css file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,24 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const postCssPurge = {
+  loader: require.resolve('postcss-loader'),
+  options: {
+    postcssOptions: {
+      plugins: [
+        require('@fullhuman/postcss-purgecss')({
+          content: ['src/**/*.js', 'lib/**/*.js'],
+          extractors: [
+            {
+              extractor: (content) => content.match(/\w+/g) || [],
+              extensions: ['js']
+            }
+          ]
+        })
+      ]
+    }
+  }
+};
 
 module.exports = {
   output: {
@@ -13,13 +33,20 @@ module.exports = {
       {
         test: /\.css$/,
         loader: [
-          require.resolve('style-loader'),
-          require.resolve('css-loader')
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {esModule: true}
+          },
+          require.resolve('css-loader'),
+          postCssPurge
         ]
       }
     ]
   },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    }),
     new HtmlWebpackPlugin({
       template: require.resolve('./index.html')
     })
