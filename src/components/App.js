15ff89285305a59c3998408f860acad4eabe6b39
Maksim Ryzhikov <rv.maksim@gmail.com>
2020-09-20 17:38:12 +0300

    refactor: remove unnecessary wrappers

--- src/components/App.js
+++ src/components/App.js
@@ -40,6 +40,26 @@ function AppBody() {
 function AppHeader() {
   const ctrl = useController();
 
+  const searchPanel = e('div', {className: classNames(t.w_full, t.relative)},
+    e('div', {className: classNames(t.absolute, t.inset_y_0, t.pl_4, t.left_0, t.flex, t.items_center)},
+      e(SearchIcon, {className: classNames(t.w_5, t.text_gray_600)})
+    ),
+    e('input', {
+      onKeyPress: ({key, target: {value}}) => {
+        if (key === 'Enter') {
+          ctrl.search(value);
+        }
+      },
+      className: classNames(
+        t.transition_colors, t.duration_100, t.ease_in_out,
+        t.py_2, t.pr_4, t.pl_12, t.block, t.w_full,
+        t.appearance_none, t.leading_normal, t.border, t.border_transparent,
+        t.rounded_lg, t.focus_outline_none, t.text_left, t.select_none, t.truncate,
+        t.focus_bg_white, t.focus_border_gray_300, t.bg_gray_200
+      )
+    })
+  );
+
   return e('header', {
       className: classNames(
         containerCss,
@@ -48,30 +68,6 @@ function AppHeader() {
         t.inset_x_0, t.z_50, t.h_16, t.items_center
       )
     },
-    e('div', {className: classNames(t.flex, t.w_full, t.max_w_screen_xl, t.relative, t.mx_auto)},
-      e('div', {
-          className: classNames(t.flex, t.flex_grow, t.min_w_0, t.lg_w_3_4, t.xl_w_4_5)
-        },
-        e('div', {className: classNames(t.w_full, t.relative)},
-          e('div', {className: classNames(t.absolute, t.inset_y_0, t.pl_4, t.left_0, t.flex, t.items_center)},
-            e(SearchIcon, {className: classNames(t.w_5, t.text_gray_600)})
-          ),
-          e('input', {
-            onKeyPress: ({key, target: {value}}) => {
-              if (key === 'Enter') {
-                ctrl.search(value);
-              }
-            },
-            className: classNames(
-              t.transition_colors, t.duration_100, t.ease_in_out,
-              t.py_2, t.pr_4, t.pl_12, t.block, t.w_full,
-              t.appearance_none, t.leading_normal, t.border, t.border_transparent,
-              t.rounded_lg, t.focus_outline_none, t.text_left, t.select_none, t.truncate,
-              t.focus_bg_white, t.focus_border_gray_300, t.bg_gray_200
-            )
-          })
-        )
-      )
-    )
+    searchPanel
   );
 }
